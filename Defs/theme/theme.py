db4f103a719e4cb6a878a5d0fc412783d7ae7f66
Coding With Devil <69082359+codingwithdevil@users.noreply.github.com>
2022-09-05 23:05:27 +0530

    Update theme.py

--- Defs/theme/theme.py
+++ Defs/theme/theme.py
@@ -8,26 +8,15 @@ import Defs.pallette.palette as plt
 default_palette = plt.default_palette
 
 mainlogo = """
-                                                                                                                                                                                                                                                                                                                                         
-{0}IIIIIIIIIIPPPPPPPPPPPPPPPPP                    {2}TTTTTTTTTTTTTTTTTTTTTTT                                                                                               
-{0}I::::::::IP::::::::::::::::P                   {2}T:::::::::::::::::::::T                                                                                               
-{0}I::::::::IP::::::PPPPPP:::::P                  {2}T:::::::::::::::::::::T                                                                                               
-{0}II::::::IIPP:::::P     P:::::P                 {2}T:::::TT:::::::TT:::::T                                                                                               
-{0}  I::::I    P::::P     P:::::P                 {2}TTTTTT  T:::::T  TTTTTTrrrrr   rrrrrrrrr   aaaaaaaaaaaaa      cccccccccccccccc    eeeeeeeeeeee    rrrrr   rrrrrrrrr   
-{0}  I::::I    P::::P     P:::::P                 {2}        T:::::T        r::::rrr:::::::::r  a::::::::::::a   cc:::::::::::::::c  ee::::::::::::ee  r::::rrr:::::::::r  
-{0}  I::::I    P::::PPPPPP:::::P                  {2}        T:::::T        r:::::::::::::::::r aaaaaaaaa:::::a c:::::::::::::::::c e::::::eeeee:::::eer:::::::::::::::::r 
-{0}  I::::I    P:::::::::::::PP   {1}--------------- {2}        T:::::T        rr::::::rrrrr::::::r         a::::ac:::::::cccccc:::::ce::::::e     e:::::err::::::rrrrr::::::r
-{0}  I::::I    P::::PPPPPPPPP     {1}-:::::::::::::- {2}        T:::::T         r:::::r     r:::::r  aaaaaaa:::::ac::::::c     ccccccce:::::::eeeee::::::e r:::::r     r:::::r
-{0}  I::::I    P::::P             {1}--------------- {2}        T:::::T         r:::::r     rrrrrrraa::::::::::::ac:::::c             e:::::::::::::::::e  r:::::r     rrrrrrr
-{0}  I::::I    P::::P                             {2}        T:::::T         r:::::r           a::::aaaa::::::ac:::::c             e::::::eeeeeeeeeee   r:::::r            
-{0}  I::::I    P::::P                             {2}        T:::::T         r:::::r          a::::a    a:::::ac::::::c     ccccccce:::::::e            r:::::r            
-{0}II::::::IIPP::::::PP                           {2}      TT:::::::TT       r:::::r          a::::a    a:::::ac:::::::cccccc:::::ce::::::::e           r:::::r            
-{0}I::::::::IP::::::::P                           {2}      T:::::::::T       r:::::r          a:::::aaaa::::::a c:::::::::::::::::c e::::::::eeeeeeee   r:::::r            
-{0}I::::::::IP::::::::P                           {2}      T:::::::::T       r:::::r           a::::::::::aa:::a cc:::::::::::::::c  ee:::::::::::::e   r:::::r            
-{0}IIIIIIIIIIPPPPPPPPPP                           {2}      TTTTTTTTTTT       rrrrrrr            aaaaaaaaaa  aaaa   cccccccccccccccc    eeeeeeeeeeeeee   rrrrrrr            
-                                                                                                                                                                    
-                                                                                                                                    {3}Coded By Coding With Devil
-""".format(default_palette[0],default_palette[3],default_palette[1],default_palette[2])
+
+{0} _____ _____   {1} _______
+{0}|_   _|  __ \  {1}|__   __|
+{0}  | | | |__) | {1}   | |_ __ __ _  ___ ___ _ __
+{0}  | | |  ___/  {1}   | | '__/ _` |/ __/ _ \ '__|
+{0} _| |_| |      {1}   | | | | (_| | (_|  __/ |
+{0}|_____|_|      {1}   |_|_|  \__,_|\___\___|_|
+                        \033[1;33mCoded By Devil
+""".format(default_palette[0],,default_palette[1])
 
 
 minilogo ="""\033[1;32m 
