5263b21801cebd9f36b2e5505ee32b286da82e23
Coding With Devil <69082359+codingwithdevil@users.noreply.github.com>
2022-09-05 23:09:16 +0530

    Update IP-Tracer.py

--- IP-Tracer.py
+++ IP-Tracer.py
@@ -1,8 +1,9 @@
 #
 #    IP-Tracer   by Coding With Devil  https://www.youtube.com/c/CodingWithDevil/
-#    This tool is used for Trace Ip address Location
+#    This tool is used for Trace Ip address Location 
+#    Also this tool is only for Education Purpose only
 #    This program comes with ABSOLUTELY NO WARRANTY
-#    The use of the YTE & its resources is COMPLETE RESPONSIBILITY of the END-USER.
+#    The use of the Ip-Tracer & its resources is COMPLETE RESPONSIBILITY of the END-USER.
 #    Developers assume NO liability and are NOT responsible for any damage caused by this program.
 
 import Defs.runner.runner as main_runner
